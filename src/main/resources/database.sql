30d8914155cecf69e5a30b72bc8cf367a6f08134
Ronald <r0nald19202006@gmail.com>
2025-12-01 12:07:06 +0100

    Entrega Ronald base de datos (#23)
    * Add ReynaApi, ReynaEntity, ReynaRepository, and ReynaService classes for motivational phrases management
    
    * Implement CRUD operations and pagination for ReynaEntity in ReynaApi and ReynaService
    
    * Update ReynaApi endpoint to /frasesmotivacionales and change ReynaEntity ID generation strategy to IDENTITY
    
    * Add rellenaBlog endpoint to ReynaApi and implement createRandom method in ReynaService for random phrase generation
    
    * Remove unused import and delete test endpoint from ReynaApi
    
    * Fix AUTO_INCREMENT value for frasesmotivacionales table in database.sql
    
    ---------
    
    Co-authored-by: “rond88” <“r0nald19202006@gmail.com”>
    Co-authored-by: Rafael Aznar <rafaaznar@gmail.com>

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -134,7 +134,17 @@ CREATE TABLE `ideas` (
 -- Table structure for table `pallas`
 --
 
-CREATE TABLE `pallas` (
+This branch has conflicts that must be resolv
+
+-- --------------Ronald------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `frasesmotivacionales`
+--
+
+
+
+  CREATE TABLE `calinesculistacompra` (
   `id` bigint NOT NULL,
   `titulo` varchar(100) DEFAULT NULL,
   `contenido` longtext,
@@ -386,3 +396,46 @@ ALTER TABLE `soares`
 ALTER TABLE `tarea`
   MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
 COMMIT;
+
+-- --------------Ronald------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `frasesmotivacionales`
+--
+
+CREATE TABLE `frasesmotivacionales` (
+  `id` bigint NOT NULL,
+  `frase` varchar(1024) COLLATE utf32_unicode_ci NOT NULL,
+  `autor` varchar(255) COLLATE utf32_unicode_ci NOT NULL DEFAULT 'Anónimo',
+  `es_publica` tinyint(1) NOT NULL,
+  `fecha_creacion` datetime NOT NULL,
+  `fecha_modificacion` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `frasesmotivacionales`
+--
+
+INSERT INTO `frasesmotivacionales` (`id`, `frase`, `autor`, `es_publica`, `fecha_creacion`, `fecha_modificacion`) VALUES
+(1, 'Hola', 'Anónimo', 1, '2025-11-26 11:24:55', NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `frasesmotivacionales`
+--
+ALTER TABLE `frasesmotivacionales`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `frasesmotivacionales`
+--
+ALTER TABLE `frasesmotivacionales`
+
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
